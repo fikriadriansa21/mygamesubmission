c378af586ab5854ac8757d09ebb29faa53a48744
fikriadriansa21 <fikriadriansa21@gmail.com>
2020-07-11 22:46:09 +0700

    uicollectionview genre, uicollectionview list game auto layout done

---
+++ mygamesubmission/Controller/Genre/GenreCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  GenreCollectionViewCell.swift
+//  mygamesubmission
+//
+//  Created by Fikri Adriansa Yudha on 11/07/20.
+//  Copyright © 2020 Fikri Adriansa Yudha. All rights reserved.
+//
+
+import UIKit
+
+class GenreCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

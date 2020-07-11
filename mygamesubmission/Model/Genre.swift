dc9687c1c718a0c3ebe472a993cc21f175ff0b8f
fikriadriansa21 <fikriadriansa21@gmail.com>
2020-07-11 23:35:56 +0700

    model data genre and games

---
+++ mygamesubmission/Model/Genre.swift
@@ -0,0 +1,60 @@
+//
+//  Genre.swift
+//  mygamesubmission
+//
+//  Created by Fikri Adriansa Yudha on 11/07/20.
+//  Copyright © 2020 Fikri Adriansa Yudha. All rights reserved.
+//
+
+import Foundation
+
+struct GenreResult: Codable{
+    var results : [Genres]
+    
+    enum CodingKeys: String, CodingKey {
+        case results = "results"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try! decoder.container(keyedBy: CodingKeys.self)
+
+        results = try container.decode([Genres].self, forKey: .results)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(results, forKey: .results)
+    }
+    
+}
+
+struct Genres: Codable {
+    let id: Int?
+    let name: String?
+    let image: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case image = "image_background"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try! decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        image = try container.decode(String.self, forKey: .image)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(image, forKey: .image)
+    }
+    
+}
+
